2643d18e645dd38ba49c3d2f4462548ebfa6e8fe
Jonathan Ortiz <jortiz25131@yahoo.com>
2023-10-31 19:18:44 +0000

    adding macro to get payment types for use in int_payment_type_amount_per_order

--- models/intermediate/int_payment_type_amount_per_order.sql
+++ models/intermediate/int_payment_type_amount_per_order.sql
@@ -1,13 +1,31 @@
-with order_payments as (select * from {{ ref("stg_stripe_order_payments") }})
+{# declaration of payment_type variable. Add here if a new one appears #}
+{%- set payment_types = get_payment_types() -%}
 
-select
-    order_id,
-    sum(
-        case when payment_type = 'cash' and status = 'success' then amount else 0 end
-    ) as cash_amount,
-    sum(
-        case when payment_type = 'credit' and status = 'success' then amount else 0 end
-    ) as credit_amount,
-    sum(case when status = 'success' then amount end) as total_amount
-from order_payments
-group by order_id
+with
+
+    payments as (select * from {{ ref("stg_stripe_order_payments") }}),
+
+    pivot_and_aggregate_payments_to_order_grain as (
+
+        select
+            order_id,
+            {% for payment_type in payment_types -%}
+
+                sum(
+                    case
+                        when payment_type = '{{ payment_type }}' and status = 'success'
+                        then amount
+                        else 0
+                    end
+                ) as {{ payment_type }}_amount,
+            {% endfor -%}
+            sum(case when status = 'success' then amount end) as total_amount
+
+        from payments
+
+        group by 1
+
+    )
+
+select *
+from pivot_and_aggregate_payments_to_order_grain
