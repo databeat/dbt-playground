05fd5ee1fc2a947eb4ef8f2ded519c5c155e121d
Jonathan Ortiz <jortiz25131@yahoo.com>
2023-10-31 21:02:01 +0000

    Updating get_payment_types macro to use generic get_distinct_column_values base macro

--- macros/get_payment_types.sql
+++ macros/get_payment_types.sql
@@ -1,18 +1,6 @@
+{# Macro to get the distinct payment_types #}
 {% macro get_payment_types() %}
-    {% set payment_type_query %}
-    select distinct payment_type
-    from {{ ref('stg_stripe_order_payments') }}
-    order by 1
-    {% endset %}
 
-    {% set results = run_query(payment_type_query) %}
-
-    {% if execute %}
-        {# Return the first column #}
-        {% set results_list = results.columns[0].values() %}
-    {% else %} {% set results_list = [] %}
-    {% endif %}
-
-    {{ return(results_list) }}
+    {{ return(get_distinct_column_values("payment_type", ref("stg_stripe_order_payments"))) }}
 
 {% endmacro %}
