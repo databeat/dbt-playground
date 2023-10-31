faf1f207fb46ba7da37770af6f31709ba6fc11c2
Jonathan Ortiz <jortiz25131@yahoo.com>
2023-10-31 19:52:06 +0000

    updating macro for scalability

--- macros/get_payment_types.sql
+++ macros/get_payment_types.sql
@@ -1,3 +1,18 @@
 {% macro get_payment_types() %}
-    {{ return(["cash", "credit"]) }}
+    {% set payment_type_query %}
+    select distinct payment_type
+    from {{ ref('stg_stripe_order_payments') }}
+    order by 1
+    {% endset %}
+
+    {% set results = run_query(payment_type_query) %}
+
+    {% if execute %}
+        {# Return the first column #}
+        {% set results_list = results.columns[0].values() %}
+    {% else %} {% set results_list = [] %}
+    {% endif %}
+
+    {{ return(results_list) }}
+
 {% endmacro %}
