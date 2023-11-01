25afeac0dbc9bd002c5c6b07604838102dd2a47f
Jonathan Ortiz <jortiz25131@yahoo.com>
2023-11-01 03:01:02 +0000

    updating dbt_utils_experimentation

--- analyses/dbt_utils_experimentation.sql
+++ analyses/dbt_utils_experimentation.sql
@@ -1,6 +1,17 @@
+-- create a date_spine
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2023-01-01' as date)",
     end_date="cast('2023-02-01' as date)"
    )
 }}
+
+-- experiment with safe_divide
+select
+    order_id,
+    customer_id,
+    cash_amount,
+    total_amount,
+    {{ dbt_utils.safe_divide('cash_amount', 'total_amount') }}
+from {{ ref('fct_orders') }}
+
